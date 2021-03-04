8cc3c0d3e443d29eb97a08a1847bb3a0a5888a5a
ARX05205 <antonina.chepa@arcavita.it>
2021-03-04 15:57:10 +0100

    test qualità non negativa

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,23 +6,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-//    All items have a SellIn value which denotes the number of days we have to sell the item
-//	- All items have a Quality value which denotes how valuable the item is
-//	- At the end of each day our system lowers both values for every item
     @Test
     void degradaQualitaESellinOgniGiorno() {
-        Item[] items = new Item[]{new Item("genericItem", 10, 9)};
-        GildedRose app = new GildedRose(items);
+        GildedRose app = appWithItem("genericItem", 10, 9);
         app.updateQuality();
         assertEquals(9, app.items[0].sellIn);
         assertEquals(8, app.items[0].quality);
     }
+
+//    All items have a SellIn value which denotes the number of days we have to sell the item
+//	- All items have a Quality value which denotes how valuable the item is
 //    - Once the sell by date has passed, Quality degrades twice as fast
-//	- The Quality of an item is never negative
 //	- "Aged Brie" actually increases in Quality the older it gets
 //	- The Quality of an item is never more than 50
 //            - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
 //	- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
 
+    @Test
+    void controllaQualitaNonNegativa() {
+        GildedRose app = appWithItem("genericItem", 10, 0);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+    }
 
+
+    private GildedRose appWithItem(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        return new GildedRose(items);
+    }
 }
